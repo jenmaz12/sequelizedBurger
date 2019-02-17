06316cee9d7ed9a2d0a069da2aec58daad1d64e7
jenmaz12 <jenmaz12@gmail.com>
2019-02-17 10:49:06 -0500

    models

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 var PORT = process.env.PORT || 8080;
 
 var app = express();
+var db = require('./models');
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -23,8 +24,11 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+// Syncing sequelize models and Start our server so that it can begin listening to client requests.
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+})
+
