775ab3fe48fc8fa4917e498d9d658861affbb212
jenmaz12 <jenmaz12@gmail.com>
2019-02-20 22:23:36 -0500

    controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,44 +1,44 @@
-// var express = require("express");
-// var db = require("../models");
+var express = require("express");
+var db = require("../models");
 
-// var router = express.Router();
+var router = express.Router();
 
-// router.get("/", function (req, res) {
-//   db.Burger.findAll({})
-//     .then(function (data) {
-//         var hbsObject = {
-//             burgers: data
-//           };
-//         console.log(hbsObject);
-//         res.render("index", hbsObject);
-//     });
-// });
+router.get("/", function (req, res) {
+  db.Burger.findAll({})
+    .then(function (data) {
+        var hbsObject = {
+            burgers: data
+          };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
 
-// router.post("/api/burgers", function (req, res) {
-//   db.Burger.create({
-//     burger_name: req.body.burger_name,
-//   }).then(function (result) {
-//     // Send back the ID of the new quote
-//     res.json({ id: result.insertId });
+router.post("/api/burgers", function (req, res) {
+  db.Burger.create({
+    burger_name: req.body.burger_name,
+  }).then(function (result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
     
-//   });
-// });
+  });
+});
 
-// router.put("/api/burgers/:id", function (req, res) {
-//   var condition = "id = " + req.params.id;
+router.put("/api/burgers/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
 
-//   console.log("condition", condition);
+  console.log("condition", condition);
 
-//   db.Burger.update({
-//     devoured: req.body.devoured
-//   }, condition, function (result) {
-//     if (result.changedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
+  db.Burger.update({
+    devoured: req.body.devoured
+  }, condition, function (result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
 
-// module.exports = router;  
+module.exports = router;  
--- server.js
+++ server.js
@@ -20,8 +20,11 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-require("./routes/api-routes.js")(app);
-require("./routes/html-routes.js")(app);
+// require("./routes/api-routes.js")(app);
+// require("./routes/html-routes.js")(app);
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
 
 // Syncing sequelize models and Start our server so that it can begin listening to client requests.
 db.sequelize.sync(syncOptions).then(function() {
